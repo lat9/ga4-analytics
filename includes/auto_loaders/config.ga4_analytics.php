971c68a901db5e8ee7a59fad81f46830742833c9
lat9 <lat9@vinosdefrutastropicales.com>
2022-12-05 08:00:20 -0500

    Restructuring of base EC Analytics, prior to start ...
    ... of GA4 conversion.

--- includes/auto_loaders/config.ga4_analytics.php
+++ includes/auto_loaders/config.ga4_analytics.php
@@ -7,8 +7,12 @@
 * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
 * @version $Id: config.ec_analytics.php 2017-05-18 15:47:36Z webchills $
 */
-$autoLoadConfig[90][] = array('autoType'=>'class',
-'loadFile'=>'observers/class.ec_analytics.php');
-$autoLoadConfig[90][] = array('autoType'=>'classInstantiate',
-'className'=>'ec_analytics',
-'objectName'=>'ec_analytics');
+$autoLoadConfig[90][] = [
+    'autoType' => 'class',
+    'loadFile' => 'observers/class.ga4_analytics.php'
+];
+$autoLoadConfig[90][] = [
+    'autoType' => 'classInstantiate',
+    'className' => 'ga4_analytics',
+    'objectName' => 'ga4_analytics'
+];
--- includes/templates/YOUR_TEMPLATE/jscript/jscript_ga4_analytics.php
+++ includes/templates/YOUR_TEMPLATE/jscript/jscript_ga4_analytics.php
@@ -9,9 +9,10 @@
  */
 
 if ((!defined('GOOGLE_UA') || GOOGLE_UA === "UA-XXXXXXXX-X")) { 
-        echo '<script>alert("The Google Analytics trackingID is not yet defined in\n /includes/extra_datafiles/ec_analytics.php")</script>' ;    
-    
-  } else { $trackingID = GOOGLE_UA ; }
+        echo '<script>alert("The Google Analytics trackingID is not yet defined in\n /includes/extra_datafiles/ec_analytics.php")</script>';
+} else {
+    $trackingID = GOOGLE_UA;
+}
 ?>
 <script>
     (function (i, s, o, g, r, a, m) {
@@ -20,33 +21,33 @@ if ((!defined('GOOGLE_UA') || GOOGLE_UA === "UA-XXXXXXXX-X")) {
             (i[r].q = i[r].q || []).push(arguments);
         }, i[r].l = 1 * new Date();
         a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
+        m = s.getElementsByTagName(o)[0];
         a.async = 1;
         a.src = g;
         m.parentNode.insertBefore(a, m);
     })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
 <?php
 //global $analytics, $cID;
-$cID = (isset($_SESSION['customer_id'])) ? "customerID#".$_SESSION['customer_id']:"guest";
-echo ($cID === "guest") ? "ga('create', '".$trackingID."', 'auto') ;\n":"ga('create', '".$trackingID."', {'userId':'{$cID}'});\n";   
-echo "ga('require', 'ec');\n"; 
+$cID = (isset($_SESSION['customer_id'])) ? 'customerID#' . $_SESSION['customer_id'] : 'guest';
+echo ($cID === 'guest') ? "ga('create', '" . $trackingID . "', 'auto') ;\n" : "ga('create', '" . $trackingID . "', {'userId':'{$cID}'});\n";
+echo "ga('require', 'ec');\n";
 echo "ga('require', 'displayfeatures');\n";
-if (!isset($_SESSION['analytics'])) {  echo "ga('send', 'pageview');\n";} 
+if (!isset($_SESSION['analytics'])) {
+    echo "ga('send', 'pageview');\n";
+} else {
+    function _output($step) { 
+        global $analytics, $cID;
 
-else {      
-function _output($step) { 
-    global $analytics, $cID; 
-                echo $analytics['addProductItemsStr'] ;   
-                echo "ga('ec:setAction','checkout', { 'step': '{$step}' , 'list': 'View Info' , 'option': '{$analytics['action']}' });\n";  
-                echo "ga('send', 'pageview');\n";
-                             
-                echo "ga('send', 'event', '{$analytics['action']}' , '{$analytics['action']}' , '{$cID}');\n";    
-}
-        
- $analytics = $_SESSION['analytics'];  
+        echo $analytics['addProductItemsStr'];
+        echo "ga('ec:setAction','checkout', { 'step': '{$step}' , 'list': 'View Info' , 'option': '{$analytics['action']}' });\n";
+        echo "ga('send', 'pageview');\n";
+
+        echo "ga('send', 'event', '{$analytics['action']}' , '{$analytics['action']}' , '{$cID}');\n";
+    }
+
+    $analytics = $_SESSION['analytics'];
     switch ($analytics['action']) {
-        case "View Product" :
+        case 'View Product':
             if (isset($analytics['item']['productID'])) {
                 echo "ga('ec:addImpression', {'id': '{$analytics['item']['productID']}' , 'name': '{$analytics['item']['productName']}' , 'category': '{$analytics['item']['category']}' , 'brand': '{$analytics['item']['brand']}' , 'list': 'View Info' , 'dimension1': '{$cID}'} ); \n";
                 echo "ga('ec:setAction', 'detail');\n";
@@ -55,38 +56,51 @@ function _output($step) {
             }
             break;
 
-        case "Add to Cart" :
-        case "Delete from Cart":
+        case 'Add to Cart':
+        case 'Delete from Cart':
             if (isset($analytics['item']['productID'])) {
-                $action = ($analytics['action'] == "Add to Cart") ? "add" : "remove";
+                $action = ($analytics['action'] == 'Add to Cart') ? 'add' : 'remove';
                 echo "ga('ec:addProduct', {'id': '{$analytics['item']['productID']}' , 'name': '{$analytics['item']['productName']}', 'quantity': '{$analytics['item']['productQTY']}' , 'category': '{$analytics['item']['category']}' , 'brand': '{$analytics['item']['brand']}', 'variant': '{$analytics['item']['variant']}' , 'dimension1': '{$cID}'} );\n";
                 echo "ga('ec:setAction', '{$action}' , { 'list':'View Info' } );\n";
                 echo "ga('send', 'pageview');\n";
                 echo "ga('send', 'event', '{$analytics['action']}' ,'{$analytics['item']['productName']}' , '{$cID}');\n";
-                }
+            }
             break;
-            
+
 //  Checkout steps
-        case "Start Login":                         _output(1) ; break;
-        case "Login Success": 
-        case "Login Success via Create Account": 
-        case "Login Success via No account":        _output(2) ; break; 
-            case "Checkout Process begin":              _output(3) ; break; // Only used with the FEC/COWOA checkout mod.                        
-            case "Checkout Shipping":                   _output(3) ; break; // Only used with the Standard checkout. 
-        case "Checkout Payment":                    _output(4) ; break;  
-        case "Checkout Confirmation":               _output(5) ; break;  
-            
-        case "Checkout Success":  // Record as a Transaction 
-            if ((is_array($analytics['transaction'])) && $analytics['transaction']['id'] != "") {
+        case 'Start Login':
+            _output(1);
+            break;
+        case 'Login Success':
+        case 'Login Success via Create Account':
+        case 'Login Success via No account':
+            _output(2);
+            break;
+        case 'Checkout Process begin':
+            _output(3);
+            break; // Only used with the FEC/COWOA checkout mod.
+        case 'Checkout Shipping':
+            _output(3);
+            break; // Only used with the Standard checkout.
+        case 'Checkout Payment':
+            _output(4);
+            break;
+        case 'Checkout Confirmation':
+            _output(5);
+            break;  
+        case 'Checkout Success':  // Record as a Transaction 
+            if ((is_array($analytics['transaction'])) && $analytics['transaction']['id'] != '') {
                 echo "ga('send', 'event', '{$analytics['action']}' , 'Order#{$analytics['transaction']['id']}','{$cID}' );\n"; //  simple event //
                 echo "ga('ec:setAction', 'purchase', { 'id': '{$analytics['transaction']['id']}','affiliation': '{$analytics['transaction']['affiliation']}', 'revenue': '{$analytics['transaction']['revenue']}', 'tax': '{$analytics['transaction']['tax']}','shipping': '{$analytics['transaction']['shipping']}','coupon': '{$analytics['transaction']['coupon']}', 'dimension1': '{$cID}' });\n";
-                echo $analytics['addProductItemsStr'] ;                    
-                echo "ga('send', 'pageview');\n";  
-        }
-        break;
-        default: echo "ga('send', 'event', '{$analytics['action']}', '{$analytics['action']}' , '{$cID}');\n";//  simple event
+                echo $analytics['addProductItemsStr'];
+                echo "ga('send', 'pageview');\n";
+            }
+            break;
+        default:
+            echo "ga('send', 'event', '{$analytics['action']}', '{$analytics['action']}' , '{$cID}');\n";//  simple event
+            break;
     }
-unset($_SESSION['analytics']);
+    unset($_SESSION['analytics']);
 }
 ?>
 </script>
