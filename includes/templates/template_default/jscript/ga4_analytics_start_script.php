7d01fae9c4520678d457282bf9785474e458fa3f
lat9 <lat9@vinosdefrutastropicales.com>
2022-12-14 09:47:48 -0500

    #2: Include 'user_id' when customer is logged in
    ... set to `$_SESSION['customer_id']`

--- includes/templates/template_default/jscript/ga4_analytics_start_script.php
+++ includes/templates/template_default/jscript/ga4_analytics_start_script.php
@@ -7,6 +7,15 @@
 // /includes/classes/observers/class.ga4_analytics.php, so long as the GA4 Analytics is currently
 // enabled.
 //
+// -----
+// If a non-guest customer is logged-in, send the 'user_id' parameter as the customer's 'customer_id'.  See
+// this (https://developers.google.com/analytics/devguides/collection/ga4/user-id/?platform=websites) google
+// documentation for additional information.
+//
+$user_id_parameters = '';
+if (zen_is_logged_in() && !zen_in_guest_checkout()) {
+    $user_id_parameters = ', ' . json_encode(['user_id' => (string)$_SESSION['customer_id']]);
+}
 ?>
 <!-- Google tag (gtag.js) -->
  <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $ga4_measurement_id; ?>"></script>
@@ -14,5 +23,5 @@
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', '<?php echo $ga4_measurement_id; ?>');
+    gtag('config', '<?php echo $ga4_measurement_id; ?>'<?php echo $user_id_parameters; ?>);
 </script>
