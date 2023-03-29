31a4bc7e7b107c680b15efc68ae520b85c3163e3
lat9 <lat9@vinosdefrutastropicales.com>
2023-03-29 17:47:26 -0400

    #13: Load observer *after* currencies are present

--- includes/auto_loaders/config.ga4_analytics.php
+++ includes/auto_loaders/config.ga4_analytics.php
@@ -1,4 +1,11 @@
 <?php
+// -----
+// Part of the "GA4 Analytics" plugin, created by lat9 (https://vinosdefrutastropicales.com)
+// Copyright (c) 2022-2023, Vinos de Frutas Tropicales.
+//
+// Last updated: v1.1.1
+//
+// Based on:
 /**
 * @package Google Enhanced E-Commerce Analytics
 * @copyright (c) 2015 RodG
@@ -7,11 +14,11 @@
 * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
 * @version $Id: config.ec_analytics.php 2017-05-18 15:47:36Z webchills $
 */
-$autoLoadConfig[90][] = [
+$autoLoadConfig[120][] = [
     'autoType' => 'class',
     'loadFile' => 'observers/class.ga4_analytics.php'
 ];
-$autoLoadConfig[90][] = [
+$autoLoadConfig[120][] = [
     'autoType' => 'classInstantiate',
     'className' => 'ga4_analytics',
     'objectName' => 'ga4_analytics'
