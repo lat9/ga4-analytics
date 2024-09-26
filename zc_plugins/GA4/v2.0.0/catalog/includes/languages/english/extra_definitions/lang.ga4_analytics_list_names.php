58ccf4bc75b01430a9cb774d4e32d65d24e96d6c
Chris Brown <code@drbyte.dev>
2024-09-26 15:03:50 -0400

    Use correct format for language file

---
+++ zc_plugins/GA4/v2.0.0/catalog/includes/languages/english/extra_definitions/lang.ga4_analytics_list_names.php
@@ -0,0 +1,16 @@
+<?php
+// -----
+// Part of the "GA4 Analytics" plugin, created by lat9 (https://vinosdefrutastropicales.com)
+// Copyright (c) 2022, Vinos de Frutas Tropicales.
+//
+// -----
+// Definitions used in the 'view_item_list' events for various product listings.
+// The values are used as the 'item_list_name'.
+//
+return [
+    'GA4_ANALYTICS_FEATURED_PRODUCTS' => 'Featured Products',
+    'GA4_ANALYTICS_ALL_PRODUCTS' => 'All Products',
+    'GA4_ANALYTICS_NEW_PRODUCTS' => 'New Products',
+    'GA4_ANALYTICS_SPECIALS' => 'Specials',
+    'GA4_ANALYTICS_PRODUCTS_LISTING' => 'Products Listing',
+];
